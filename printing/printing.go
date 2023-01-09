7eebb38de5724a060806e796dc1ed36c38946e6e
Otavio Cantareli <otavio_cantareli@outlook.com>
2023-01-09 15:02:32 -0300

    Lint

--- printing/printing.go
+++ printing/printing.go
@@ -3,22 +3,22 @@ package main
 import "fmt"
 
 func main() {
-    // Print RGB values...
-    r, g, b := 124, 87, 3
+	// Print RGB values...
+	r, g, b := 124, 87, 3
 
-    // ...as #7c5703  (specifying hex format, fixed width, and leading zeroes)
-    // Hint: don't forget to add a newline at the end of the format string.
-    fmt.Printf("#%2x%2x%02x\n", r, g, b)
+	// ...as #7c5703  (specifying hex format, fixed width, and leading zeroes)
+	// Hint: don't forget to add a newline at the end of the format string.
+	fmt.Printf("#%2x%2x%02x\n", r, g, b)
 
-    // ...as rgb(124, 87, 3)
-    fmt.Printf("rgb(%d, %d, %d)\n", r, g, b)
+	// ...as rgb(124, 87, 3)
+	fmt.Printf("rgb(%d, %d, %d)\n", r, g, b)
 
-    // ...as rgb(124, 087, 003) (specifying fixed width and leading zeroes)
-    fmt.Printf("rgb(%d, %03d, %03d)\n", r, g, b)
+	// ...as rgb(124, 087, 003) (specifying fixed width and leading zeroes)
+	fmt.Printf("rgb(%d, %03d, %03d)\n", r, g, b)
 
-    // ...as rgb(48%, 34%, 1%) (specifying a literal percent sign)
-    // fmt.Printf("", r, g, b)
+	// ...as rgb(48%, 34%, 1%) (specifying a literal percent sign)
+	// fmt.Printf("", r, g, b)
 
-    // Print the type of r.
-    fmt.Printf("%T\n", r)
+	// Print the type of r.
+	fmt.Printf("%T\n", r)
 }
